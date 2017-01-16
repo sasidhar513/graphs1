6bec4a39fcd76a7736306ee87ce5138118ef84ce
sasidhar513 <sasidhar.dulipudi@tcs.com>
2017-01-16 19:37:18 +0530

    Update GraphTraversal.cpp

--- GraphTraversal.cpp
+++ GraphTraversal.cpp
@@ -30,6 +30,12 @@ class Graph
     Graph(int vertices);
     void addEdge(int source ,int destination);
     void BFS(int start);
+    void DFS(int start);
+    void printDFS(int start,bool *check);
+    bool DetectCycleDirected();
+    bool DCDUtil(int start,bool * check,bool *loop);
+  
+
 };
 Graph::Graph(int vertices)
 {
@@ -64,20 +70,86 @@ void Graph::BFS(int start)
         }    
     }
 }
+void Graph::DFS(int start)
+{
+    bool *visited=new bool[vertices];
+    for(int i=0;i<vertices;i++)
+        visited[i]=false;
+    printDFS(start,visited);
+    
+}
+void Graph::printDFS(int start,bool *visited)
+{
+    if(visited[start]==true)
+        return;
+    cout<<start<<" ";
+    visited[start]=true;
+    for(list<int>::iterator i=adj[start].begin();i!=adj[start].end();i++)
+    {
+        printDFS(*i,visited);
+    }
+    return;
+}
+
+bool Graph::DetectCycleDirected()
+{
+    bool *check=new bool[vertices];
+    bool *loop=new bool[vertices];
+    bool out=false;
+    for(int i=0;i<vertices;i++)
+    {
+        check[i]=false;
+        loop[i]=false;
+    }
+    for(int i=0;i<vertices;i++)
+    {
+        if(check[i]==false)
+            out=DCDUtil(i,check,loop);
+        if(out==true)
+            break;
+    }
+    return out;
+}
+bool Graph::DCDUtil(int start,bool *check,bool *loop)
+{
+    //cout<<endl<<"start " << start<<endl;
+    if(loop[start]==true)
+        return true;
+    if(check[start]==true)
+        return false;
+    check[start]=true;
+    loop[start]=true;
+    
+    bool out=false;
+    for(list<int>::iterator i=adj[start].begin();i!=adj[start].end();i++)
+    {        
+        out=DCDUtil(*i,check,loop);
+        if(out==true)
+            return out;
+    }
+    loop[start]=false;
+    return out;
+}
+
 int main()
 { 
      // Create a graph given in the above diagram
     Graph g(4);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
+
     g.addEdge(1, 2);
-    g.addEdge(2, 0);
+
+    //g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
+
  
     cout << "Following is Breadth First Traversal "
          << "(starting from vertex 2) \n";
     g.BFS(2);
+    g.DFS(2);
+    cout<<g.DetectCycleDirected();
     return 0;
 
 }
