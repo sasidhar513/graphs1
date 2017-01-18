9f71d6c66c3f470032cab44508074608f86e4ef8
sasidhar513 <sasidhar.dulipudi@tcs.com>
2017-01-18 19:33:17 +0530

    Update GraphTraversal.cpp

--- GraphTraversal.cpp
+++ GraphTraversal.cpp
@@ -20,38 +20,70 @@
 #include <fstream>
 #include <ctime>
 #include <cassert>
-
-using namespace std; // }}}
-class Graph
+using namespace std; 
+//Adjacent node of a Node with v(vertex name) and distance with default 0
+struct AdjacentNode
 {
-    int vertices;
-    list<int> *adj;
-    public:
-    Graph(int vertices);
-    void addEdge(int source ,int destination);
-    void BFS(int start);
-    void DFS(int start);
-    void printDFS(int start,bool *check);
-    bool DetectCycleDirected();
-    bool DCDUtil(int start,bool * check,bool *loop);
-  
-
+    int v,distance=0;
 };
-Graph::Graph(int vertices)
-{
-    this->vertices=vertices;
-    adj=new list<int>[vertices];
+/*  Graph class that handles both directed Graph and Undirected Graph 
+    Default is  Directed Graph
+    for Undirected Graph pass false as second parameter to the constructor
+*/
+class Graph
+{    
+    int vertices;
+    bool isDirected=true;
+    list<AdjacentNode> *adj;
+    void DFSutil(int start,bool *check);
+    bool DCDUtil(int start,bool * check,bool *loop);
+    public:
+    //Constructor for Graph class
+    Graph(int vertices,bool isDirected=true);
+    //Adding an Edge with or without distance
+    void addEdge(int source ,int destination,int dist=0);
+    //Breadth First Search  using the Queue
+    void BFS(int start);
+    //Depth First Search using the recursion
+    void DFS(int start);
+    //Returns true if there are any cycles 
+    bool DetectCycleDirected();
+};
+Graph::Graph(int vertices,bool isDirected)
+{   
+    this->vertices=vertices;//number of vertices
+    this->isDirected=isDirected;//is Directed or not
+    adj=new list<AdjacentNode>[vertices];//Initialising the array of list of Adjacent node 
 }
-void Graph::addEdge(int src,int dest)
+void Graph::addEdge(int src,int dest,int dist)
 {
-    adj[src].push_back(dest);
+    //creating the node and pushing the node in the src node list
+    AdjacentNode node;
+    node.v=dest;
+    node.distance=dist;
+    adj[src].push_back(node);
+    if(!isDirected)//if the graph id Undirected 
+    {        
+        AdjacentNode node1;
+        node1.v=src;
+        node1.distance=dist;
+        adj[dest].push_back(node1);        
+    }
 }
 void Graph::BFS(int start)
 {
+     /*logic:    1 push the root node in queue  and check of root is true
+                2 loop until the queue is empty 
+                    pop element 
+                    insert all the adjacent nodes to the queue and check of node is true 
+                    print the popped element
+    */
+    //creating Check array which verifies the vertex is visited or not 
     bool *check=new bool[100];
+    //initilising the array false 
     for(int i=0;i<100;i++)
         check[i]=false;
-    queue<int> q;
+    queue<int> q;   
     q.push(start);
     check[start]=true;
     int temp;
@@ -60,39 +92,52 @@ void Graph::BFS(int start)
         temp=q.front();
         q.pop();
         cout<<temp<<" ";
-        for(list<int>::iterator i=adj[temp].begin();i!=adj[temp].end();i++)
+        for(list<AdjacentNode>::iterator i=adj[temp].begin();i!=adj[temp].end();i++)
         {
-            if(check[*i]==false)
+            if(check[(*i).v]==false)
             {
-                q.push(*i);
-                check[*i]=true;   
+                q.push((*i).v);
+                check[(*i).v]=true;   
             }
         }    
     }
 }
 void Graph::DFS(int start)
 {
+    /*logic :
+                1 print root node and visited is true 
+                2 loop through all adjacent nodes 
+                3       if adjacent node is not visited 
+                            go to step one with root being the adjacent node
+                        else return 
+    */
     bool *visited=new bool[vertices];
     for(int i=0;i<vertices;i++)
         visited[i]=false;
-    printDFS(start,visited);
+    DFSutil(start,visited);
     
 }
-void Graph::printDFS(int start,bool *visited)
+void Graph::DFSutil(int start,bool *visited)
 {
     if(visited[start]==true)
         return;
     cout<<start<<" ";
     visited[start]=true;
-    for(list<int>::iterator i=adj[start].begin();i!=adj[start].end();i++)
-    {
-        printDFS(*i,visited);
-    }
+    for(list<AdjacentNode>::iterator i=adj[start].begin();i!=adj[start].end();i++)
+        DFSutil((*i).v,visited);
     return;
 }
-
 bool Graph::DetectCycleDirected()
-{
+{    
+    /*logic:
+                1 Take two arrays one for visited and other for checking if the path is loop or not 
+                2 take root node
+                3 if loop value is already true then  return true                
+                4 set the visited and loop values true
+                5 loop for all the Adjacent nodes 
+                    repeat step 2 with root being the adjacent node
+                6 return false
+    */     
     bool *check=new bool[vertices];
     bool *loop=new bool[vertices];
     bool out=false;
@@ -112,7 +157,6 @@ bool Graph::DetectCycleDirected()
 }
 bool Graph::DCDUtil(int start,bool *check,bool *loop)
 {
-    //cout<<endl<<"start " << start<<endl;
     if(loop[start]==true)
         return true;
     if(check[start]==true)
@@ -121,26 +165,23 @@ bool Graph::DCDUtil(int start,bool *check,bool *loop)
     loop[start]=true;
     
     bool out=false;
-    for(list<int>::iterator i=adj[start].begin();i!=adj[start].end();i++)
+    for(list<AdjacentNode>::iterator i=adj[start].begin();i!=adj[start].end();i++)
     {        
-        out=DCDUtil(*i,check,loop);
+        out=DCDUtil((*i).v,check,loop);
         if(out==true)
             return out;
     }
     loop[start]=false;
     return out;
 }
-
 int main()
 { 
      // Create a graph given in the above diagram
-    Graph g(4);
+    Graph g(4,false);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-
     g.addEdge(1, 2);
-
-    //g.addEdge(2, 0);
+    g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
 
@@ -148,7 +189,7 @@ int main()
     cout << "Following is Breadth First Traversal "
          << "(starting from vertex 2) \n";
     g.BFS(2);
-    g.DFS(2);
+
     cout<<g.DetectCycleDirected();
     return 0;
 
